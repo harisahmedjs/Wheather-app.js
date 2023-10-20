2c20030b0293c1cd77a132f6f20a6e0d32fd1aa2
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2023-10-20 07:54:00 +0500

    completed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const div =document.querySelector('.div')
 
 form.addEventListener('submit' , (e)=>{
 e.preventDefault();
-axios.get(`http://api.weatherapi.com/v1/current.json?key=7d605a9992aa473da80153421231710&q=${input.value}`)
+axios.get(`https://api.weatherapi.com/v1/current.json?key=7d605a9992aa473da80153421231710&q=${input.value}`)
 .then((res)=>{
     const dataa =res.data
     console.log(dataa)
