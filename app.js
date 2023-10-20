ac9a2adf9033578822f9d73d3511f3e3a62fcbdc
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2023-10-20 07:51:52 +0500

    completed

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ form.addEventListener('submit' , (e)=>{
 e.preventDefault();
 axios.get(`http://api.weatherapi.com/v1/current.json?key=7d605a9992aa473da80153421231710&q=${input.value}`)
 .then((res)=>{
-
-    console.log(res.data)   
+    const dataa =res.data
+    console.log(dataa)
+    
+div.innerHTML +=`<div class ="div-js">
+<h1 class="name">${dataa.location.name}</h1>
+<p class='date'>${dataa.location.localtime} , ${dataa.location.country}</p>
+<div class="center">
+ <img src="${dataa.current.condition.icon}" class="image">
+ <p class="text">${dataa.current.condition.text}</p>
+</div>
+</div>`
+       
 })
 .catch((err)=>{
    console.log(err)
